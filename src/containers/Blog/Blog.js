05d016575bf1bc8e4c75a627bc6702a2264660e4
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-04-14 21:56:49 +0530

    WIP: Setting up react package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -33,15 +31,18 @@ class Blog extends Component {
         });
         return (
             <div>
+                <header>
+                    <nav>
+                        <ul className="Posts">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
-                    <FullPost id = {this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                
             </div>
         );
     }
