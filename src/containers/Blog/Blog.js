b554286177e2e28878c9e988dbaf59c5c794ea5e
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-04-16 11:53:46 +0530

    Link and Route implementation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import Posts from '../../containers/Blog/Posts/Posts';
+import {Route, Link} from 'react-router-dom';
 
+import Posts from '../../containers/Blog/Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -12,13 +14,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul className="Posts">
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{ 
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true' 
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
-                
+                {/* <Route path="/" exact render = { () => <h1>Home</h1> }/>
+                <Route path="/" exact render = { () => <h1>Home</h1> }/> */}
+                <Route path="/" exact component = {Posts} />
+                <Route path="/new-post" component = {NewPost} />
             </div>
         );
     }
