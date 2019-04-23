364850e50d87d07bf18cbec6050dab4c8ba6ff4a
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-04-23 12:29:54 +0530

    Fixing the exact route rendering using Switch component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import Posts from '../../containers/Blog/Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -26,9 +26,11 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render = { () => <h1>Home</h1> }/>
                 <Route path="/" exact render = { () => <h1>Home</h1> }/> */}
-                <Route path="/" exact component = {Posts} />
-                <Route path="/new-post" exact component = {NewPost} />
-                <Route path="/:id" exact component = {FullPost} />
+                <Switch>
+                    <Route path="/" exact component = {Posts} />
+                    <Route path="/new-post" exact component = {NewPost} />
+                    <Route path="/:id" exact component = {FullPost} />
+                </Switch>
             </div>
         );
     }
